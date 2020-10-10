ac39b889881ad9d27befc43c4b124ae148646661
bakalianosm <sdi1700269@di.uoa.gr>
2020-10-10 20:32:16 +0300

    edited to work without leaks

--- modules/inverted_index.c
+++ modules/inverted_index.c
@@ -14,15 +14,15 @@ struct index_node{
     int year;
     List indexList;
 };
-void rec_destroy(Record a){
-    free(a->firstName);
-    free(a->lastName);
-    free(a);
+void rec_destroy(Pointer a){
+    free( ((Record)a)->firstName);
+    free( ((Record)a)->lastName);
+    free( ((Record)a));
 }
-void indexNode_destroy(indexNode a){
-
-    list_destroy(a->indexList);
-    free(a);
+void indexNode_destroy(Pointer a){
+    
+    list_destroy( ((indexNode)a)->indexList);
+    free(((indexNode)a));
 }
 
 int compare_index_nodes(Pointer a, Pointer b){
@@ -32,7 +32,7 @@ int compare_index_nodes(Pointer a, Pointer b){
 
 invertedIndex invertedIndex_create(){
     invertedIndex ii = malloc(sizeof(*ii));
-    ii->list = list_create(NULL);
+    ii->list = list_create(indexNode_destroy);
     
 
     return ii;
@@ -44,50 +44,9 @@ int invertedIndex_size(invertedIndex ii){
 
 void invertedIndex_insert(invertedIndex ii , Record rec){
     
-
     
     // First we have to find in which list it has to be stored.
 
-    
-
-    // if(invertedIndex_size(ii)==0){
-
-    //     printf("Empty inverted index\n");
-
-    //     indexNode new = malloc(sizeof(*new));
-    //     new->year = rec->year;
-    //     new->indexList = list_create(NULL);
-
-    //     list_insert_next(new->indexList,LIST_EOF, rec);
-    //     ii->size++;
-
-    // }
-    // else{
-    //     printf("Entering in non empty inverted index\n");
-    //     indexNode temp =malloc(sizeof(*temp));
-    //     temp->year = rec->year;
-        
-    //     indexNode iNode = (indexNode)list_find_node(ii->list, temp, compare_index_nodes);
-
-    //     if(iNode!=NULL){
-    //         printf("Exists node with this year.Insert the record");
-    //         List l = (List)iNode->indexList;
-    //         list_insert_next(l, LIST_EOF, rec);
-            
-    //     }
-    //     else{
-    //         printf("Doesn't exist node with this year.Let's create it!\n");
-    //         indexNode new = malloc(sizeof(*new));
-    //         new->year = rec->year;
-    //         new->indexList = list_create(NULL);
-    //         list_insert_next(new->indexList, LIST_EOF, rec);
-    //         list_insert_next(ii->list, LIST_EOF, new);
-
-    //     }
-
-    // }
-
-    
     printf("Inverted index inserting\n");
     indexNode toFind = malloc(sizeof(*toFind));
     toFind->year =rec->year;
@@ -104,6 +63,7 @@ void invertedIndex_insert(invertedIndex ii , Record rec){
         printf("Doesn't exist node with this year.Let's create it!\n");
         indexNode new = malloc(sizeof(*new));
         new->year = rec->year;
+        // new->indexList = list_create(rec_destroy);
         new->indexList = list_create(NULL);
         list_insert_next(new->indexList, LIST_EOF, rec);
         list_insert_next(ii->list, LIST_EOF, new);
@@ -113,7 +73,7 @@ void invertedIndex_insert(invertedIndex ii , Record rec){
 
 
     // list_destroy(toFind->indexList);
-    // free(toFind);
+    free(toFind);
 
     // list_destroy(toFind->indexList);
     // free(toFind);
@@ -121,10 +81,7 @@ void invertedIndex_insert(invertedIndex ii , Record rec){
 
 
 void invertedIndex_destroy(invertedIndex ii){
-
-    
     
     list_destroy(ii->list);
     free(ii);
-
 }
