d3e775168f16e210620b068f884b978abc762778
bakalianosm <sdi1700269@di.uoa.gr>
2020-10-12 21:10:40 +0300

    added functionality

--- include/inverted_index.h
+++ include/inverted_index.h
@@ -5,13 +5,13 @@
 #pragma once
 #include "common_types.h"
 #include "linked_list.h"
-
+#include <stdio.h>
 
 typedef struct inverted_index* invertedIndex;
 typedef struct index_node* indexNode;
 
 // a function to compare inverted index nodes
-int compare_index_nodes(Pointer a,Pointer b);
+int compare_index_nodes(Pointer a, Pointer b);
 
 // This function simply allocates the necessary memory that
 // needed to store the inverted index
@@ -23,3 +23,21 @@ int invertedIndex_size(invertedIndex ii);
 
 // This function inserts the given record inside the proper list 
 void invertedIndex_insert(invertedIndex ii , Record record);
+
+//This function deallocate the memory that the inverted index allocated
+void invertedIndex_destroy(invertedIndex ii);
+
+// This function is used for searching a record 
+bool invertedIndex_find(Record rec);
+
+// This function is used to delete a record
+void invertedIndex_delete(invertedIndex ii,Record rec);
+
+
+// This function returns the count of records are stored for the year
+int invertedIndex_yearCount(invertedIndex ii, int year);
+
+
+// This function returns the count of records are stored for each year
+void invertedIndex_count(invertedIndex ii);
+
