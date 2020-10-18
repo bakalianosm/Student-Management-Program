a48a33cc6f011711fca16032eae427b18f5b54e2
bakalianosm <michalianos99@gmail.com>
2020-10-18 03:10:42 +0300

    Comments added

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,110 +1,67 @@
-// #pragma once
-// #include "common_types.h"
- 
-// // dummy nodes - just for error detection 
-// #define LIST_BOF (ListNode)0 // node before the first (;)
-// #define LIST_EOF (ListNode)0 // node after the last (;)
 
-// typedef struct linked_list* LinkedList;
-// typedef struct list_node* ListNode;
-
-
-// // Liked List functionality 
-
-// // 1. This function is used to make a dummy list, just to allocate the 
-// // necessary memory to hold an [empty] list.
-
-// LinkedList list_create();
-
-
-// // 2. Thus function is used to insert elements in the list, after the 
-// // node "node" or at the top of the list if the node is LIST_BOF with 
-// // value == value .
-
-// void list_insert(LinkedList list, ListNode node, Pointer value);
-
-//////////////////////////////////////////////////////////////////
-//
-// ADT List
-//
-// Abstract λίστα. Παρέχει σειριακή πρόσβαση στα στοιχεία, και
-// προσθήκη/αφαίρεση σε οποιοδήποτε σημείο της λίστας.
-//
-///////////////////////////////////////////////////////////////////
-
-#pragma once // #include το πολύ μία φορά
-
+#pragma once 
 #include "common_types.h"
 
-// Οι σταθερές αυτές συμβολίζουν εικονικούς κόμβους _πριν_ τον πρώτο και _μετά_ τον τελευταίο
-#define LIST_BOF (ListNode)0
-#define LIST_EOF (ListNode)0
+
+#define LIST_BOF (ListNode)0 // the node before the first
+#define LIST_EOF (ListNode)0 // the node after the last 
 
 
-// Λίστες και κόμβοι αναπαριστώνται από τους τύπους List και ListNode. Ο χρήστης δε χρειάζεται να γνωρίζει το περιεχόμενο
-// των τύπων αυτών, απλά χρησιμοποιεί τις συναρτήσεις list_<foo> που δέχονται και επιστρέφουν List / ListNode.
-//
-// Οι τύποι αυτοί ορίζινται ως pointers στα "struct list" και "struct list_node" των οποίων το
-// περιεχόμενο είναι άγνωστο (incomplete structs), και εξαρτάται από την υλοποίηση του ADT List.
-//
 typedef struct list* List;
 typedef struct list_node* ListNode;
 
-
-
-// Δημιουργεί και επιστρέφει μια νέα λίστα.
-// Αν destroy_value != NULL, τότε καλείται destroy_value(value) κάθε φορά που αφαιρείται ένα στοιχείο.
+// If the destroy value != NULL then every time we remove an item
+// the destroy_value(value) function is called
 
 List list_create(DestroyFunc destroy_value);
 
-// Επιστρέφει τον αριθμό στοιχείων που περιέχει η λίστα.
+// returns the size of the list
 
 int list_size(List list);
 
-// Προσθέτει έναν νέο κόμβο __μετά__ τον node, ή στην αρχή αν node == LIST_BOF, με περιεχόμενο value.
-
+// Inserts a new node after the given node or at the start of the list
+// if the node is LIST_BOF with "value"
 void list_insert_next(List list, ListNode node, Pointer value);
 
-// Αφαιρεί τον __επόμενο__ κόμβο από τον node, ή τον πρώτο κόμβο αν node == LIST_BOF.
-
+// Removes the next of the "node" or the first node if the given node is LIST_BOF
 void list_remove_next(List list, ListNode node);
 
-// Επιστρέφει την πρώτη τιμή που είναι ισοδύναμη με value
-// (με βάση τη συνάρτηση compare), ή NULL αν δεν υπάρχει
+
+// Returns the first node that has "value" value based on  the compare function 
 
 Pointer list_find(List list, Pointer value, CompareFunc compare);
 
-// Αλλάζει τη συνάρτηση που καλείται σε κάθε αφαίρεση/αντικατάσταση στοιχείου σε
-// destroy_value. Επιστρέφει την προηγούμενη τιμή της συνάρτησης.
-
+// Changes the function tha is called when we remove or replace an element 
+// to destroy_value and returns the past destroy_function
 DestroyFunc list_set_destroy_value(List list, DestroyFunc destroy_value);
 
-// Ελευθερώνει όλη τη μνήμη που δεσμεύει η λίστα list.
-// Οποιαδήποτε λειτουργία πάνω στη λίστα μετά το destroy είναι μη ορισμένη.
+// Frees all the memory that the list has allocated 
 
 void list_destroy(List list);
 
 
-// Διάσχιση της λίστας /////////////////////////////////////////////
+// List Iterate /////////////////////////////////////////////
 //
-// Επιστρέφουν τον πρώτο και τον τελευταίο κομβο της λίστας, ή LIST_BOF / LIST_EOF αντίστοιχα αν η λίστα είναι κενή
+
+// Returns the first node of the list, or LIST_EOF if the list is empty
 
 ListNode list_first(List list);
+
+// Returns the last node of the list, or LIST_BOF if the list is empty
 ListNode list_last(List list);
 
-// Επιστρέφει τον κόμβο μετά από τον node, ή LIST_EOF αν ο node είναι ο τελευταίος
+// Returns the node after the "node" or LIST_EOF if the "node" hasn't next
 
 ListNode list_next(List list, ListNode node);
 
-// Επιστρέφει το περιεχόμενο του κόμβου node
+// Returns the "value" of the "node"
 
 Pointer list_node_value(List list, ListNode node);
 
-// Βρίσκει τo πρώτo στοιχείο που είναι ισοδύναμο με value (με βάση τη συνάρτηση compare).
-// Επιστρέφει τον κόμβο του στοιχείου, ή LIST_EOF αν δεν βρεθεί.
+
+// Finds the first element that has "value" value due to compare function
+// Returns the node that contains the "value" either LIST_EOF if don't exists
+// node with "value" value
 
 ListNode list_find_node(List list, Pointer value, CompareFunc compare);
 
-
-// Deletes the node node of the list.
-// void list_remove(List list, Pointer value, CompareFunc compare);
